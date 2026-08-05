5ed00e5c54f9d960b5617a76c990a714e3591c1f
Tom Price <tkprice@mtu.edu>
2026-08-05 13:34:34 -0400

    Updated Readme. I have a final version good to go, including instructions for downloading the font used (since it isn't mine to license)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Tiny-Balatro-Maker
-This program takes a Balatro.exe from Steam and installs a mod to make it more playable on tiny screens (640x480, tested on a 2.8 inch screen but will work on larger).
+This program takes a Balatro.exe from Steam and installs a mod to make it more playable on tiny screens (640x480, tested on a 2.8 inch screen but will work on larger). Note that I am not a good programmer, so this is what I have so far. 
+Instructions:
+-Download this repo to a folder of your choice
+-Find your Steam version of "Balatro.exe" and copy it to the root of this project folder (same location as "tinyBalatroMaker.m")
+-Download the font at this link: https://www.atozfont.com/download/noto-sans-condensed-bold
+-Extract the font and drag it to the "Images" folder (so "Images/NotoSans-CondensedBold.ttf" exists)
+-Run "tinyBalatroMaker.m"
+
+This will create a new version of "Balatro.exe" that can be dragged into the Portmaster folder, or can be used in creating an apk for Andriod installations. 
+
--- tinyBalatroMaker.m
+++ tinyBalatroMaker.m
@@ -52,8 +52,8 @@ miscTxt{1761} = [miscTxt{1761} '*1.13'];
 cardareaTxt{285} = [cardareaTxt{285}(1:101) '42' cardareaTxt{285}(103:end)]; % card count font scale
 cardareaTxt{286} = [cardareaTxt{286}(1:62)  '42' cardareaTxt{286}(64:end)];  % slash font scale
 cardareaTxt{287} = [cardareaTxt{287}(1:101) '42' cardareaTxt{287}(103:end)]; % card limit font scale
-% 
-% % adjust location and font size of peek deck indicator WORKS
+ 
+% adjust location and font size of peek deck indicator WORKS
 cardareaTxt{371} = [cardareaTxt{371}(1:72) '2' cardareaTxt{371}(73:end)];    % increase padding from 0.1 to 0.12
 cardareaTxt{372} = [cardareaTxt{372}(1:199) '72' cardareaTxt{372}(201:end)]; % increase button scale from 0.6 to 0.72
 cardareaTxt{374} = [cardareaTxt{374}(1:77) '576' cardareaTxt{374}(80:end)];  % increase "peek" text from 0.48 to 0.576
@@ -62,64 +62,42 @@ cardareaTxt{381}(57) = '3'; % change x offset from x=-0.5 to x=-0.3
 cardareaTxt{381}(61) = '1'; % change y offset from y=0.1 to y=1.1
 % 
 % % adjust location of blind marquee WORKS
-blindTxt{126}    = [blindTxt{126}(1:end-1) '-1.5']; % change y offset
-blindTxt{748}    = [blindTxt{748}(1:end-1) '-1.5']; % change y offset
-% 
-% % adjust joker and consumable spots WORKS
+blindTxt{126}    = [blindTxt{126}(1:end-1) '-0.8']; % change y offset
+blindTxt{748}    = [blindTxt{748}(1:end-1) '-0.8']; % change y offset
+ 
+% adjust joker, deck and consumable spots WORKS
 commonTxt{9}     = [commonTxt{9}(1:end-5) '+ 0.2']; % change x offset of jokers
-commonTxt{10}    = [commonTxt{10}(1:end-1) '-1.0']; % change y offset of jokers
-commonTxt{13}    = [commonTxt{13}(1:end-1) '-1.0']; % change x offset of consumables
+commonTxt{10}    = [commonTxt{10}(1:end-1) '-1.9']; % change y offset of jokers
+commonTxt{13}    = [commonTxt{13}(1:end-1) '-1.9']; % change y offset of consumables
 commonTxt{15}    = [commonTxt{15}(1:end-1) '5'];    % change x offset of deck
-% 
-% % remove rotation of the cards by setting the card.T.r values to 0 WORKS
-% cardareaTxt{420} = cardareaTxt{420}(1:32);
-% cardareaTxt{432} = [cardareaTxt{432}(1:27) '0'];
+
+% reduced number of the cash out dots, was causing issues
+commonTxt{940} = [ commonTxt{940}(1:78) '........' commonTxt{940}(117:end)];
+
+% remove rotation of the cards by setting the card.T.r values to 0 WORKS
 cardareaTxt{439} = cardareaTxt{439}(1:28);
 cardareaTxt{454} = cardareaTxt{454}(1:28);
 cardareaTxt{469} = cardareaTxt{469}(1:28);
 cardareaTxt{484} = cardareaTxt{484}(1:28);
 cardareaTxt{512} = cardareaTxt{512}(1:28);
-% 
-% % flatten cards in hand to all have the same height WORKS
+
+% flatten cards in hand to all have the same height WORKS
 cardareaTxt{444} = [cardareaTxt{444}(1:73) '- 0.3'];
 cardareaTxt{460} = [cardareaTxt{460}(1:81) '- 0.2'];
 cardareaTxt{474} = [cardareaTxt{474}(1:80) cardareaTxt{474}(end-28:end)];
 cardareaTxt{486} = [cardareaTxt{486}(1:180) cardareaTxt{486}(end-19:end)];
 cardareaTxt{489} = [cardareaTxt{489}(1:81) cardareaTxt{489}(end-28:end)];
-% 
-% swap x and y to match switch controls DOESN'T WORK
-% globalsTxt{30}(30:34) = 'true ';
-% UIdefTxt{6131} = [UIdefTxt{6132}(1:12) 'G.F_SWAP_XY_BUTTONS and 3 or 2,'];
-% UIdefTxt{6132} = [UIdefTxt{6132}(1:12) 'G.F_SWAP_XY_BUTTONS and 2 or 3,'];
 
 % increase button size by 40%
 UIdefTxt{6149} = [UIdefTxt{6149}(1:35) '(' UIdefTxt{6149}(36:53) ...
     ')*1.4,(' UIdefTxt{6149}(55:72) ')*1.4,' UIdefTxt{6149}(74:end)] ;
 % 
-% % shift Play/Discard buttons down and to the left WORKS
+% % shift Play/Discard buttons down and to the left 
 buttonCBTxt{832} = [buttonCBTxt{832}(1:44) '-0.9' ...
     buttonCBTxt{832}(46:48) '1.3' buttonCBTxt{832}(52:end)];
 gameTxt{3049} = [gameTxt{3049}(1:46) '-0.9' gameTxt{3049}(48:50) ...
     '1.3' gameTxt{3049}(54:end)]; 
  
-% increase size of Play/Discard buttons by 20%  - don't need it
-% UIdefTxt{987} = [UIdefTxt{987} '*1.2'];
-% UIdefTxt{988} = [UIdefTxt{988} '*1.2'];
-% UIdefTxt{989} = [UIdefTxt{989}(1:88) '*1.2' UIdefTxt{989}(89:103) ...
-%     '*1.2' UIdefTxt{989}(104:end)];
-% UIdefTxt{995} = [UIdefTxt{995}(1:96) '*1.2' UIdefTxt{995}(97:117) ...
-%     '*1.2' UIdefTxt{995}(118:end)];
-% 
-% % increase size of Rank/Suit sorting by 20%  - don't need it
-% UIdefTxt{1002} = [UIdefTxt{1002}(1:64) '*1.2' UIdefTxt{1002}(65:79)...
-%     '*1.2' UIdefTxt{1002}(80:end)];
-% UIdefTxt{1005} = [UIdefTxt{1005}(1:57) '*1.2' UIdefTxt{1005}(58:end)];
-% UIdefTxt{1010} = [UIdefTxt{1010}(1:61) '*1.2' UIdefTxt{1010}(62:end)];
-% UIdefTxt{1011} = [UIdefTxt{1011}(1:60) '*1.2' UIdefTxt{1011}(61:72)...
-%     '*1.2' UIdefTxt{1011}(73:87) '*1.2' UIdefTxt{1011}(88:end)];
-% UIdefTxt{1014} = [UIdefTxt{1014}(1:60) '*1.2' UIdefTxt{1014}(61:72) ...
-%     '*1.2' UIdefTxt{1014}(73:87) '*1.2' UIdefTxt{1014}(88:end)];
-% 
 % increase size of the Sell and Use buttons, adjust for no overlap
 % minw = 1.3, 'sell' and 1.9 or 1.4
 UIdefTxt{422} = [UIdefTxt{422}(1:84) '.3' UIdefTxt{422}(85:158) ...
@@ -133,26 +111,38 @@ UIdefTxt{438}(138:140) = '1.3';
 UIdefTxt{438}(169:171) = '1.3';
 % 
 % % move main menu UI elements down
-commonTxt{758} = [commonTxt{758}(1:end-1) '1.6'];
-commonTxt{767} = [commonTxt{767}(1:65) '1.6' commonTxt{767}(67:end)];
+commonTxt{758} = [commonTxt{758}(1:end-1) '1.9'];
+commonTxt{767} = [commonTxt{767}(1:65) '1.9' commonTxt{767}(67:end)];
 % 
 % increase size of main menu logo
-gameTxt{1569}  = [gameTxt{1569}(1:23) '21' gameTxt{1569}(25:end)];
+gameTxt{1569}  = [gameTxt{1569}(1:23) '4' gameTxt{1569}(25:end)];
 
 % move version number up
 gameTxt{1712}  = [gameTxt{1712}(1:47) '-1.6' gameTxt{1712}(49:end)];
-%adjust width of Joker and Consumable boxes
+
+% adjust width of Joker and Consumable boxes
 gameTxt{2228} = [gameTxt{2228}(1:18) '5.29' gameTxt{2228}(22:end)];
 gameTxt{2230} = [gameTxt{2230}(1:24) '1.96' gameTxt{2230}(28:end)];
+
+% move shop item costs up. 
+UIdefTxt{875}(88) = '1';
+UIdefTxt{875}(95:96) = '10';
+
+% increase minimum height of hand display 
+UIdefTxt{1341} = [UIdefTxt{1341}(1:98) 'minh = 3.2, ', UIdefTxt{1341}(99:end)];
+
+% shift blind alignment down
+UIdefTxt{1405} = [UIdefTxt{1405}(1:80) '.2' UIdefTxt{1405}(84:end)];
+
 
 % overwrite the default cards with the "Grandma mode" cards in resources
 movefile('UnzipLocation/resources/textures/1x/8BitDeck_opt2.png',...
     'UnzipLocation/resources/textures/1x/8BitDeck_opt2OLD.png')
-copyfile('Images/BalatroGrandmaH1a.png',...
+copyfile('Images/BalatroGrandmaH1b.png',...
     'UnzipLocation/resources/textures/1x/8BitDeck_opt2.png')
 movefile('UnzipLocation/resources/textures/2x/8BitDeck_opt2.png',...
     'UnzipLocation/resources/textures/2x/8BitDeck_opt2OLD.png')
-copyfile('Images/BalatroGrandmaH2a.png',...
+copyfile('Images/BalatroGrandmaH2b.png',...
     'UnzipLocation/resources/textures/2x/8BitDeck_opt2.png')
 
 %% write files
@@ -161,7 +151,9 @@ writelines(gameTxt,gameFN);
 writelines(miscTxt,miscFN);
 writelines(globalsTxt,globalsFN);
 writelines(UIdefTxt,UIdefFN);
-writelines(blindTxt,blindFN)
+writelines(blindTxt,blindFN);
+writelines(commonTxt,commonFN);
+writelines(buttonCBTxt,buttonCBFN);
 
 %% zip everything and create the exe
 zip('Balatro.zip','*','UnzipLocation');
